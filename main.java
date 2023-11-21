ebbfbb3db09731f3c44cee101428ee11325d84ec
Youssef Ait Mellouk <147152506+youssefaim@users.noreply.github.com>
2023-11-21 02:17:02 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String[] args) {
+        Employer emp1 = new Employer() ;
+        emp1.number = 1 ;
+        emp1.name = "youssef" ;
+        emp1.sales = 345 ;
+        emp1.bonushrs = 1004 ;
+        emp1.basesalary = 14000 ;
+
+        emp1.info();
+    }
+}
